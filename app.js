35ce2c52f4941a9818ba2e69ccf30a29b3381492
dhimasarista <dhimasharrisst@gmail.com>
2023-07-31 10:10:42 +0700

    add: controllers/

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require('path');
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
