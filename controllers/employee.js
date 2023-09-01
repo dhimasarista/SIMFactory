11a4a5a079b48bee87b99bf7092fef1c042eeb04
dhimasarista <dhimasharrisst@gmail.com>
2023-09-01 11:42:19 +0700

    Fixed upload problem

--- controllers/employee.js
+++ controllers/employee.js
@@ -71,62 +71,62 @@ const employee = {
     },
     updateEmploye: async (req, res) => {
         try {
-        const idToUpdate = req.params.id;
-        const { 
-            name, 
-            department_id, 
-            address, 
-            number_phone, 
-            email, 
-            last_education, 
-            major, 
-            title, 
-            work_experience, 
-            skills, 
-            mcu, criminal_history,
-            // file
-            photo,
-            application_letter,
-            CV,
-            portfolio,
-            employmentContract 
-        } = req.body;
-        
-
-        // Kueri
-        const queryUsers = `UPDATE users SET department_id = ? WHERE id = ?`;
-        const queryEmployee = `SELECT * FROM employees WHERE id = ?`;
-        const queryUpdateEmployee = `UPDATE employees SET ? WHERE id = ?`;
-        
-        const imageFile = photo === undefined ? null : fs.readFileSync(`uploads/images/${photo}`);
-        const appLetterFile = application_letter === undefined ? null : fs.readFileSync(`uploads/pdfs/${application_letter}`);
-        const CVFile = CV === undefined ? null : fs.readFileSync(`uploads/pdfs/${CV}`);
-        // Destructing Array of Object
-        const [employeeOldData] = await queryAsync(queryEmployee, [idToUpdate]);
-        // Melakukan update ke tabel users
-        await queryAsync(queryUsers, [department_id, idToUpdate]);
-        const data = {
-            name: name === undefined ? employeeOldData.name : name,
-            department_id: department_id === undefined ? employeeOldData.department_id : parseInt(department_id),
-            address: address === undefined ? employeeOldData.address : address,
-            number_phone: number_phone === undefined ? employeeOldData.number_phone : number_phone,
-            email: email === undefined ? employeeOldData.email : email,
-            last_education: last_education === undefined ? employeeOldData.last_education : last_education,
-            major: major === undefined ? employeeOldData.major : major,
-            title: title === undefined ? employeeOldData.title : title,
-            work_experience: work_experience === undefined ? employeeOldData.work_experience : work_experience,
-            skills: skills === undefined ? employeeOldData.skills : skills,
-            mcu: mcu === undefined ? employeeOldData.mcu : mcu,
-            criminal_history: criminal_history === undefined ? employeeOldData.criminal_history : criminal_history,
+            const idToUpdate = req.params.id;
+            const { 
+                name, 
+                department_id, 
+                address, 
+                number_phone, 
+                email, 
+                last_education, 
+                major, 
+                title, 
+                work_experience, 
+                skills, 
+                mcu, criminal_history,
+                // file
+                photo,
+                application_letter,
+                CV,
+                portfolio,
+                employmentContract 
+            } = req.body;
             
-            photo: imageFile === null ? employeeOldData.photo : imageFile,
-            application_letter: appLetterFile === null ? employeeOldData.application_letter : appLetterFile,
-            CV: CVFile === null ? employeeOldData.CV : CVFile,
-            // portfolio: portfolio === undefined ? employeeOldData.portfolio : portfolio,
-            // employmentContract: employmentContract === undefined ? employeeOldData.employmentContract : employmentContract
-        }
-        const results = await queryAsync(queryUpdateEmployee, [data, idToUpdate]);
-        res.status(200).send(results);
+
+            // Kueri
+            const queryUsers = `UPDATE users SET department_id = ? WHERE id = ?`;
+            const queryEmployee = `SELECT * FROM employees WHERE id = ?`;
+            const queryUpdateEmployee = `UPDATE employees SET ? WHERE id = ?`;
+            
+            const imageFile = photo === undefined ? null : fs.readFileSync(`uploads/images/${photo}`);
+            const appLetterFile = application_letter === undefined ? null : fs.readFileSync(`uploads/pdfs/${application_letter}`);
+            const CVFile = CV === undefined ? null : fs.readFileSync(`uploads/pdfs/${CV}`);
+            // Destructing Array of Object
+            const [employeeOldData] = await queryAsync(queryEmployee, [idToUpdate]);
+            // Melakukan update ke tabel users
+            await queryAsync(queryUsers, [department_id, idToUpdate]);
+            const data = {
+                name: name === undefined ? employeeOldData.name : name,
+                department_id: department_id === undefined ? employeeOldData.department_id : parseInt(department_id),
+                address: address === undefined ? employeeOldData.address : address,
+                number_phone: number_phone === undefined ? employeeOldData.number_phone : number_phone,
+                email: email === undefined ? employeeOldData.email : email,
+                last_education: last_education === undefined ? employeeOldData.last_education : last_education,
+                major: major === undefined ? employeeOldData.major : major,
+                title: title === undefined ? employeeOldData.title : title,
+                work_experience: work_experience === undefined ? employeeOldData.work_experience : work_experience,
+                skills: skills === undefined ? employeeOldData.skills : skills,
+                mcu: mcu === undefined ? employeeOldData.mcu : mcu,
+                criminal_history: criminal_history === undefined ? employeeOldData.criminal_history : criminal_history,
+                
+                photo: imageFile === null ? employeeOldData.photo : imageFile,
+                application_letter: appLetterFile === null ? employeeOldData.application_letter : appLetterFile,
+                CV: CVFile === null ? employeeOldData.CV : CVFile,
+                // portfolio: portfolio === undefined ? employeeOldData.portfolio : portfolio,
+                // employmentContract: employmentContract === undefined ? employeeOldData.employmentContract : employmentContract
+            }
+            const results = await queryAsync(queryUpdateEmployee, [data, idToUpdate]);
+            res.status(200).send(results);
         } catch (error) {
             errorLogging(error);
         }
@@ -134,3 +134,19 @@ const employee = {
 }
 
 module.exports = employee;
+
+    // Alur Upload Files (Server):
+    // - File yang diupload diclient, langsung dikirim ke direktori server `/uploads/`
+    // - Saat client melakukan submit, data yang dikirim adalah nama-nama filesnya.
+    //   Kemudian server akan memeriksa apakah file tersebut cocok dengan namanya.
+    // - Jika cocok file akan dikirim ke db bersama data-data lainnya.
+    // - Dan kemudian file-file yang di direktori uploads langsung dihapus.
+
+    // Problem #2: Upload hanya bisa sekali, jika ditimpa maka akan error(FIXED)
+    // Solusi #2: Jangan upload ketika event change, tapi setelah event tersebut
+    //            dengan pengecekan nilai ada atau tidak, jika ada kirim.
+
+    // Alur upload file (Client): Ketika event change pada input, lakukan pengecekan.
+    // Jika files[0] !== undefined, upload ke dir server dan tampilkan button delete.
+    // Alur delete file: button delete akan meminta response ke server untuk
+    // menghapus file sesuai nama file yang diminta
