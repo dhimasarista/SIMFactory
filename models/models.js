401eb00418a971e727cef88229f4eb05c1dec1ae
dhimasarista <mdhimasarista@gmail.com>
2023-09-19 04:03:32 +0700

    Add Table total_output

--- models/models.js
+++ models/models.js
@@ -7,9 +7,11 @@ const models = async (queryAsync) => {
                 id BIGINT(64) AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255) NOT NULL,
                 target_quantity BIGINT(64) NULL,
+                total_output BIGINT(64) NULL,
                 created_at TIMESTAMP
             )`
         );
+        // ALTER TABLE models ADD total_output BIGINT(64) NULL;
 
         const result = (modelsTable !== 0) ? "Already Exist" : "Created";
         console.log(green, `${symbol} Models Table: ${result}`);
