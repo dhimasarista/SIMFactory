6fa276381a434bc949e7867e9684c7fc7d0b22d1
dhimasarista <dhimasharrisst@gmail.com>
2023-10-20 15:14:15 +0700

    Add Monitoring Production Path

--- expressjs/routes/Monitoring.js
+++ expressjs/routes/Monitoring.js
@@ -1,3 +1,8 @@
+const { errorHandling, errorLogging} = require('../utils/errorHandling');
+const { promisify } = require('util');
+const pool = require("../configs/database");
+const queryAsync = promisify(pool.query).bind(pool);
+
 class Monitoring{
     constructor(app) {
         this.app = app;
@@ -19,15 +24,22 @@ class Monitoring{
         });
 
         this.app.route("/monitoring/materials")
-        .get((req, res) => {
+        .get( async (req, res) => {
             const user = req.cookies.user;
             const path = req.path;
 
-            res.render("500", {
-                user,
-                path,
-                errors: ["Belum dibuat"]
-            });
+            const queryMaterials = "SELECT * FROM materials";
+
+            try {
+                const results = await queryAsync(queryMaterials)
+                res.render("monitoring_materials", {
+                    user,
+                    path,
+                    materials: results
+                });   
+            } catch (error) {
+                errorHandling(res, user, path, error);
+            }
         });
 
         this.app.route("/monitoring/schedules")
