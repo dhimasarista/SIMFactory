081c59e878f6502c8901f81337837392e0343884
dhimasarista <dhimasharrisst@gmail.com>
2023-10-21 05:07:36 +0700

    Fixing UI

--- expressjs/routes/Monitoring.js
+++ expressjs/routes/Monitoring.js
@@ -18,11 +18,14 @@ class Monitoring{
             const user = req.cookies.user;
             const path = req.path;
 
-            res.render("500", {
-                user,
-                path,
-                errors: ["Belum dibuat"]
-            });
+            try {
+                res.render("monitoring_production", {
+                    user,
+                    path,
+                });
+            } catch (error) {
+                errorHandling(res, user, path, error);
+            }
         });
 
         this.app.route("/monitoring/materials")
@@ -47,24 +50,7 @@ class Monitoring{
                 errorHandling(res, user, path, error);
             }
         })
-        
-        // this.app.route("/monitoring/materials")
-        // .get( async (req, res) => {
-        //     const user = req.cookies.user;
-        //     const path = req.path;
 
-        //     const queryMaterials = "SELECT * FROM materials";
-        //     try {
-        //         const results = await queryAsync(queryMaterials)
-        //         res.render("monitoring_materials", {
-        //             user,
-        //             path,
-        //             materials: results
-        //         });   
-        //     } catch (error) {
-        //         errorHandling(res, user, path, error);
-        //     }
-        // });
 
         this.app.route("/monitoring/schedules")
         .get(async (req, res) => {
