b6ce54912a89687fa3b3587808bc0db755db9a9e
dhimasarista <mdhimasarista@gmail.com>
2023-10-23 08:52:50 +0700

    Change database configuration

--- expressjs/config/database.js
+++ expressjs/config/database.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
-const dbConfig = require("../config/db_config.json");
+const dbConfig = require("../config/config.json");
 
-const config = dbConfig["development"]["mysql"];
+const config = dbConfig["development"];
 // Create database pool
 const pool = mysql.createPool({
   host: config.host,
   port: config.port,
-  user: config.user,
+  user: config.username,
   password: config.password,
   database: config.database,
   waitForConnections: config.waitForConnections,
