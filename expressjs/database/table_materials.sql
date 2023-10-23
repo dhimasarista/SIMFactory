8ccf06de2696f3cf6eced5f5f2558c769b9c5d60
dhimasarista <dhimasharrisst@gmail.com>
2023-10-23 20:24:40 +0700

    Add New Column materials.type_code

--- expressjs/database/table_materials.sql
+++ expressjs/database/table_materials.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS materials(
     id BIGINT(64) AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    type_code INT(11) NOT NULL
     manufacturer VARCHAR(255) NULL,
     stocks BIGINT(64) NULL,
     updated_by VARCHAR(64) NULL,
