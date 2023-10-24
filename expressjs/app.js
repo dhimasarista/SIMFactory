d4276a1c4ba7589d3c02082571476d3a253d7738
dhimasarista <dhimasharrisst@gmail.com>
2023-10-24 14:08:06 +0700

    Commented Unused Code

--- expressjs/app.js
+++ expressjs/app.js
@@ -20,7 +20,6 @@ const { unMatchedRoutes, internalServer } = require('./middlewares/error');
 const app = express(); // Inisialisasi Aplikasi Express
 const server = http.createServer(app); // Create HTTP server
 dotenv.config(); // Load variabel environment dari file .env
-
 // Menggunakan Socket 
 // setupSocketIO(server);
 
@@ -38,17 +37,16 @@ function main() {
 
   metrics(app);
   app.use(compression()); // Kompresi HTTP Resources yang dikirimkan ke klien
-  sessionSetup(app); // Session
-  // app.use(flash());
+  // sessionSetup(app); // Session
   app.set('view engine', 'ejs'); // Mengatur View Engine ke EJS
   app.set('views', path.join(__dirname, 'views')); // Mengatur path ke folder views
   app.use(express.static(path.join(__dirname, 'public'))); // Serve static files dari folder "public" 
-  app.use(express.urlencoded({extended: true})); 
+  app.use(express.urlencoded({extended: false})); 
   app.use(express.json()); // Parsing Permintaan JSON
   app.use(cookieParser()); // Menggunakan cookie-parser
   // app.use(cacheMiddleware); // Caching secara global
   userAuthorization(app);
-
+  
   // Routes
   setupRoutes(app);
 
@@ -62,3 +60,4 @@ function main() {
     console.log(blue, `${symbol} Server started on http://localhost:${port}`);
   });
 }main();
+
