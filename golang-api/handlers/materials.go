c1311c00a037108f7990fa685304ee5a456a2236
dhimasarista <dhimasharrisst@gmail.com>
2023-10-24 12:39:51 +0700

    Add Goroutines

--- golang-api/handlers/materials.go
+++ golang-api/handlers/materials.go
@@ -10,8 +10,6 @@ import (
 )
 
 func GetMaterialsData(w http.ResponseWriter, r *http.Request) {
-	var materials []models.Material // Data yang akan dikirim
-
 	// Mendapatkan token dari header request
 	tokenString := r.Header.Get("Authorization")
 
@@ -23,26 +21,41 @@ func GetMaterialsData(w http.ResponseWriter, r *http.Request) {
 	// Mengambil data pengguna dari database MySQL
 	db := config.GetDBConnect()
 	script := "SELECT id, name, manufacturer, stocks, updated_by FROM materials"
-	rows, err := db.Query(script)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
 
-	for rows.Next() {
-		var material models.Material
-		err := rows.Scan(&material.ID, &material.Name, &material.Manufacturer, &material.Stocks, &material.UpdatedBy)
+	// Membuat channel untuk komunikasi antara goroutine
+	resultChan := make(chan []models.Material)
+	errorChan := make(chan error)
+
+	// Menjalankan goroutine untuk pengambilan data
+	var materials []models.Material
+	go func() {
+		rows, err := db.Query(script)
 		if err != nil {
-			log.Println(err)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			errorChan <- err
 			return
 		}
+		defer rows.Close()
 
-		materials = append(materials, material)
+		for rows.Next() {
+			var material models.Material
+			err := rows.Scan(&material.ID, &material.Name, &material.Manufacturer, &material.Stocks, &material.UpdatedBy)
+			if err != nil {
+				errorChan <- err
+				return
+			}
+
+			materials = append(materials, material)
+		}
+
+		resultChan <- materials
+	}()
+
+	select {
+	case materials := <-resultChan:
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(materials)
+	case err := <-errorChan:
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(materials)
 }
